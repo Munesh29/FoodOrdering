3e62f98bf3ec5828f3a1e1216614527b88a1a953
Munesh29 <muneshravi29@gmail.com>
2023-01-03 10:58:40 +0530

    Initial Commit

--- src/Address.java
+++ src/Address.java
@@ -1,31 +1,16 @@
 import java.io.Serializable;
 
 public class Address implements Serializable {
-    private String number;
-    private String street;
-    private String city;
-    private String state;
-    private int pinCode;
+    private final String number;
+    private final String street;
+    private final String city;
+    private final String state;
+    private final int pinCode;
     Address(String number,String street,String city,String state,int pinCode){
         this.number=number;
         this.street=street;
         this.city=city;
         this.state=state;
-        this.pinCode = pinCode;
-    }
-    void setDNumber(String number){
-        this.number=number;
-    }
-    void setStreet(String street){
-        this.street=street;
-    }
-    void setCity(String city){
-        this.city=city;
-    }
-    void setState(String state){
-        this.state=state;
-    }
-    void setPinCode(int pinCode){
         this.pinCode = pinCode;
     }
     String getDNumber(){
